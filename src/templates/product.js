3ba63fade9c5f6f2918a1ab2a28254afc91ade6a
Peter Koenders <pkoenders@gmail.com>
2020-06-04 22:37:45 +1200

    Update product.js

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,28 +1,44 @@
 import React from "react"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
-import Img from 'gatsby-image'
-import SimpleReactLightbox from "simple-react-lightbox"
-import { SRLWrapper } from "simple-react-lightbox"
+// import Img from 'gatsby-image'
+// import SimpleReactLightbox from "simple-react-lightbox"
+// import { SRLWrapper } from "simple-react-lightbox"
 
 
 import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
 import { Carousel } from 'react-responsive-carousel'
 
-//import productStyles from './productStyles.module.scss';
-
-
 
 const ProductPage = ({ data }) => {
+
   const product = data.shopifyProduct
 
+  console.log("Price = " + product.variants.price)
+
   // Create our number formatter.
-  const formatter = new Intl.NumberFormat('en-US', {
+  const currencyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'NZD',
   });
 
-
+  //Quantity counter 
+  function quantityCounter(e) {
+    let countThis = e.target.name
+    let inputVal = parseInt(document.querySelector(".quantity-input").value)
+    if (countThis === 'add') {
+      inputVal = inputVal + 1
+    } else {
+      inputVal = inputVal - 1
+    }
+    if (inputVal < 1) {
+      inputVal = 1
+    }
+    if (inputVal > 100) {
+      inputVal = 100
+    }
+    document.querySelector(".quantity-input").value = inputVal
+  }
 
   //Carousel props
   const getConfigurableProps = () => ({
@@ -43,7 +59,6 @@ const ProductPage = ({ data }) => {
     transitionTime: 150,
     swipeScrollTolerance: 5,
   });
-
 
   // SLB props
   const lightBoxOptions = {
@@ -77,25 +92,24 @@ const ProductPage = ({ data }) => {
     <>
       <Layout>
         <h1>{product.title}</h1>
+        <p>{currencyFormat.format(product.priceRange.maxVariantPrice.amount)}</p>
+        <p>{currencyFormat.format(product.variants.price)}</p>
+        <p>{product.variants.price}</p>
 
-        <SimpleReactLightbox>
-          <SRLWrapper options={lightBoxOptions}>
-            <Carousel {...getConfigurableProps()}>
-              {product.images.map((image, i) => (
-                <img
-                  className='medium-zoom-image'
-                  key={i}
-                  width='100%'
-                  src={image.localFile.url}
-                  alt={product.title}
-                  data-attribute='SRL'
-                  loading='lazy'
-                //style="pointerEvents: visible;"
-                />
-              ))}
-            </Carousel>
-          </SRLWrapper>
-        </SimpleReactLightbox>
+        <Carousel {...getConfigurableProps()}>
+          {product.images.map((image, i) => (
+            <img
+              className='medium-zoom-image'
+              key={i}
+              width='100%'
+              src={image.localFile.url}
+              alt={product.title}
+              //data-attribute='SRL'
+              loading='lazy'
+            //style="pointerEvents: visible;"
+            />
+          ))}
+        </Carousel>
 
         {/* <SimpleReactLightbox>
           <SRLWrapper options={lightBoxOptions}>
@@ -113,19 +127,59 @@ const ProductPage = ({ data }) => {
           </SRLWrapper>
         </SimpleReactLightbox> */}
 
+        <div className="quantity-group">
+          <label htmlFor="Quantity" className="quantity-label">Quantity:</label>
+          <div className="quantity" data-required-text="Please enter a quantity.">
+            <button
+              type="button"
+              className="quantity-decrease"
+              name="subtract"
+              onClick={quantityCounter}>
+              –
+            </button>
+            <input type="number" className="quantity-input" name="Quantity" size="2" min="1" max="100" maxLength="3" defaultValue="1" />
+            <button
+              type="button"
+              className="quantity-increase"
+              name="add"
+              onClick={quantityCounter}>
+              +
+            </button>
+          </div>
+        </div>
+
+        <p>{product.variants.title}</p>
+        <button
+          type="button"
+          className="btn-primary"
+          name="addToCart">
+          Add to cart
+        </button>
+
+        <p>{data.shopifyProduct.variants.sku}</p>
+
+        (description)
+        <div dangerouslySetInnerHTML={{ __html: product.description }} />
+        <br />
+        (descriptionHtml)
         <div dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
-        <p>{formatter.format(product.priceRange.maxVariantPrice.amount)}</p>
+
 
       </Layout>
     </>
   )
 }
 
-
-
 export const query = graphql`
   query($handle: String!) {
     shopifyProduct(handle: { eq: $handle }) {
+
+      variants {
+        price
+        sku
+        title
+      }
+
       id
       title
       handle
@@ -138,17 +192,7 @@ export const query = graphql`
         name
         values
       }
-      variants {
-        id
-        title
-        price
-        availableForSale
-        shopifyId
-        selectedOptions {
-          name
-          value
-        }
-      }
+      
       priceRange {
         minVariantPrice {
           amount
@@ -174,5 +218,4 @@ export const query = graphql`
     }
   }
 `
-
 export default ProductPage
